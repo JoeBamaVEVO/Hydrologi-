f2c0a444994abf85b217c87e44d5d6cb69ffde32
Kristian Grøsvik <kristian.grosvik04@gmail.com>
2023-08-12 03:32:46 +0200

    end of day push

---
+++ myProjects.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include "head.php" ?>
+</head>
+<body>
+    <div class="container-fluid ms-0 ps-0">
+        <div class="container-fluid float-start border ms-0 ps-0" style="min-height: 100%; max-width: 25%;" >
+            <h2>Mine Prosjekter</h2>
+            <ul class="list-group">
+                <li class="list-group-item active" aria-current="true">An active item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>|
