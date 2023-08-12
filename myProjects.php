701e07307672eac46af02d8ede2ebf4a27cc199a
JoeBamaVEVO <Kristian.grosvik04@gmail.com>
2023-08-12 05:33:49 +0200

    You can now make projects :D

--- myProjects.php
+++ myProjects.php
@@ -16,18 +16,34 @@
         <div class="container-fluid float-start border ms-0 ps-0" style="min-height: 100%; max-width: 25%;" >
             <h2>Mine Prosjekter</h2>
             <div class="list-group">
-                <a href="" class="list-group-item list-group-item-action">
+                <?php 
+                $ProjectDir = $userDir . "/projects";
+                $Projects = "";
+
+                if(is_dir($ProjectDir)){
+                    if($dh = opendir($ProjectDir)){
+                        while(($Project = readdir($dh)) !== false){
+                            if($Project != "." && $Project != ".."){
+                                $Projects .= "<a href='beregn.php?project={$Project}' class='list-group-item list-group-item-action'>{$Project}</a>";
+                            }
+                        }
+                        closedir($dh);
+                    }
+                }
+                echo $Projects;
+                ?>
+                <!-- <a href="" class="list-group-item list-group-item-action">
                     The current link item
                 </a>
                 <a href="#" class="list-group-item list-group-item-action">A second link item</a>
                 <a href="#" class="list-group-item list-group-item-action">A third link item</a>
                 <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
-                <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</a>
+                <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</a> -->
             </div>
             <form action="" method="POST">
                 <input name="newProjectName" type="text">
                 <button class="btn btn-primary float-end" type="submit" alt="" name="newProject"> 
-                    <p class="mb-0"><img src="/Bilder/Svg/plus-circle-fill.svg" alt=""> Nytt Prosjekt</p>
+                    <p class="mb-0"><img src="/Bilder/Svg/plus-circle-fill.svg" alt=""></p>
                 </button> 
             </form>
         </div>
