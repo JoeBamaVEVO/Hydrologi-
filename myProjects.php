77d7f68600eeff66e68b866dc40e782db13b17ec
JoeBamaVEVO <Kristian.grosvik04@gmail.com>
2023-08-12 17:39:16 +0200

    epic changes

--- myProjects.php
+++ myProjects.php
@@ -32,13 +32,6 @@
                 }
                 echo $Projects;
                 ?>
-                <!-- <a href="" class="list-group-item list-group-item-action">
-                    The current link item
-                </a>
-                <a href="#" class="list-group-item list-group-item-action">A second link item</a>
-                <a href="#" class="list-group-item list-group-item-action">A third link item</a>
-                <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
-                <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</a> -->
             </div>
             <form action="" method="POST">
                 <input name="newProjectName" type="text">
