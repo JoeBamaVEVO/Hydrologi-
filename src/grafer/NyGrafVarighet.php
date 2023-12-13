f556888f29eac8c33002851d3b36e13acdf6e119
JoeBamaVEVO <Kristian.grosvik04@gmail.com>
2023-12-13 15:36:45 +0100

    Added a function that makes varighetskurve data

--- src/grafer/NyGrafVarighet.php
+++ src/grafer/NyGrafVarighet.php
@@ -16,11 +16,9 @@ $MVerdiVinter = array();
 
 // Function that stores the total data, summer data and winter data in arrays.
 function GetWinterSummerData($csv){
-    
     global $MVerdi, $MVerdiSommer, $MVerdiVinter;
     
     $fileHandler = fopen($csv, "r");
-    
     while(list($MaleDato, $MaleVerdi) = fgetcsv($fileHandler, 1024, ",")) {
         array_push($MVerdi, round($MaleVerdi, 3)); // Her runder vi av Måleverdiene
         // Vi skiller mellom sommer og vinter halvår
@@ -38,29 +36,48 @@ fclose($fileHandler);
 return list($MVerdi, $MVerdiSommer, $MVerdiVinter) = array($MVerdi, $MVerdiSommer, $MVerdiVinter);
 }
 
+list($MVerdi, $MVerdiSommer, $MVerdiVinter) = GetWinterSummerData($csv);
 
-function CreateVarighetskurve(){
-  
-}
+$NewCsv = $projectDir . "/test.csv";
 
-CreateVarighetskurve();
 
-// Nå skal vi lage varighetskurve for vinter halvåret   
-$csvWrite = $projectDir . '/TabelldataVarighetVinter.csv';
-$fileWrite = fopen($csvWrite, "w");
+CreateVarighetskurve($MVerdi, $NewCsv);
 
-$AktuellVerdi = $MVerdiVinter[0];
 
-// Vi går gjennom arrayen
-foreach($MVerdiVinter as $index => $Verdi){
-    if($AktuellVerdi > $Verdi){
-        // Vi finner prosenten og skriver til fil sammen med verdien
-        $Prosent = round(($index/count($MVerdiVinter))*100, 4);
-        fputcsv($fileWrite, array($AktuellVerdi, $Prosent));
-        // Definerer AktuellVerdi til å være den nye verdien f.eks 7 -> 5
-        $AktuellVerdi = $Verdi;
+function CreateVarighetskurve($Array, $csv){
+    rsort($Array);
+    $RelevantValue = $Array[0];
+    $fileWrite = fopen($csv, "w");
+
+    foreach($Array as $index => $Value){
+        if($RelevantValue > $Value){
+            $Prosent = round(($index/count($Array))*100, 4);
+            fputcsv($fileWrite, array($RelevantValue, $Prosent));
+            $RelevantValue = $Value;
+        }
     }
 }
+
+
+
+
+
+// Nå skal vi lage varighetskurve for vinter halvåret   
+// $csvWrite = $projectDir . '/TabelldataVarighetVinter.csv';
+// $fileWrite = fopen($csvWrite, "w");
+
+// $AktuellVerdi = $MVerdiVinter[0];
+
+// // Vi går gjennom arrayen
+// foreach($MVerdiVinter as $index => $Verdi){
+//     if($AktuellVerdi > $Verdi){
+//         // Vi finner prosenten og skriver til fil sammen med verdien
+//         $Prosent = round(($index/count($MVerdiVinter))*100, 4);
+//         fputcsv($fileWrite, array($AktuellVerdi, $Prosent));
+//         // Definerer AktuellVerdi til å være den nye verdien f.eks 7 -> 5
+//         $AktuellVerdi = $Verdi;
+//     }
+// }
 
 
 function testWrite($csv){
